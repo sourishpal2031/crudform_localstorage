cdff9699e8dc1256558164045dd21993c9e15d9a
sourishpal2031 <sourishpal2031@gmail.com>
2024-03-05 11:51:49 +0530

    Initial Commit

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees=[
+{
+  id:"1",
+  Name:"sourish pal",
+  Age:"23"
+},
+{
+    id:"2",
+    Name:"rohit pal",
+    Age:"29"
+  },
+  {
+    id:"3",
+    Name:"virat pal",
+    Age:"33"
+  }
+]
+
+export default Employees
